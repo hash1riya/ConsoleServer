a4afe83d66ee973bcae284a69ec74e36ec93c73b
hash1riya <daniildrok1@gmail.com>
2024-08-01 12:56:24 +0300

    Init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleServer;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
