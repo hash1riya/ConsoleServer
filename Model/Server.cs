b861fae7bd786dd85f6f2c895e7be2d72c918ce0
hash1riya <daniildrok1@gmail.com>
2024-08-01 13:11:49 +0300

    ex-1

---
+++ Model/Server.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleServer.Model;
+public class Server
+{
+    IPEndPoint endp;
+    Socket socket;
+
+    public Socket Client;
+    public byte[] Buffer = new byte[1024];
+
+    public Server(string ip, int port)
+    {
+        this.endp = new IPEndPoint(IPAddress.Parse(ip), port);
+        this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        this.Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        this.socket.Bind(endp);
+        this.socket.Listen(10);
+    }
+    public void ClientAccept() => this.Client = this.socket.Accept();
+
+    public void Stop()
+    {
+        try
+        {
+            this.socket.Close();
+            this.Client.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+    public int Receive() => this.Client.Receive(this.Buffer);
+    public void Send(string strSend)
+    {
+        if (this.socket != null)
+            this.Client.Send(System.Text.Encoding.UTF8.GetBytes(strSend));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,54 @@
-﻿namespace ConsoleServer;
+﻿using System.Diagnostics;
+
+namespace ConsoleServer;
 internal class Program
 {
     public static void Main(string[] args)
     {
+        string ip = "127.0.0.1";
+        int port = 1024;
+        Model.Server server = new Model.Server(ip, port);
 
+        Console.WriteLine(
+            $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+            $"SYSTEM: Server was setted up. Waiting for client...");
+
+        string sendStr = "Hello client!";
+        int bytesRead;
+        while (true)
+        {
+            try
+            {
+                server.ClientAccept();
+
+                Console.WriteLine(
+                        $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                        $"SYSTEM: " +
+                        $"{server.Client.RemoteEndPoint} connected!");
+
+                bytesRead = server.Receive();
+                Console.WriteLine(
+                    $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                    $"{server.Client.RemoteEndPoint}: " +
+                    $"{System.Text.Encoding.UTF8.GetString(server.Buffer, 0, bytesRead)}");
+
+                server.Send(sendStr);
+                Console.WriteLine(
+                    $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                    $"me: " +
+                    $"{sendStr}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(
+                        $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                        $"SYSTEM: " +
+                        $"{server.Client.RemoteEndPoint} disconnected!\n");
+            }
+        }
     }
 }
